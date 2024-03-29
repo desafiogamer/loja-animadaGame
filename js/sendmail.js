2390f0f56f3a415c6c2f988d0bc2a715090c9b22
desafiogamer <joaopap1234@gmail.com>
2024-03-29 15:41:38 -0300

    atualização final

---
+++ js/sendmail.js
@@ -0,0 +1,29 @@
+var nome = document.getElementById('nome').value
+var email = document.getElementById('email').value
+var telefone = document.getElementById('telefone').value
+var assunto = document.getElementById('assunto').value
+var menssagem = document.getElementById('menssagem').value
+var btn = document.getElementById('envarMensagem')
+
+
+btn.addEventListener('click',()=>{
+    if(nome && email && telefone && assunto && menssagem){
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "me2803390@gmail.com",
+            Password : "60D37F3F9DCFCB84D6988D0BD93D76999C5C",
+            To : 'joaopap1234@gmail.com',
+            From : "me2803390@gmail.com",
+            Subject : `nome:${nome}  assunto:${assunto} telefone:${telefone} email:${email}`,
+            Body : `${menssagem}`
+        }).then(
+            message => alert(message)
+        );
+    }else{
+        alert('Preencha os campos')
+    }
+})
+
+
+
+
