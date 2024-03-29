807bfb3feead8bbd44012bbc4d2b7a7e222a7654
desafiogamer <joaopap1234@gmail.com>
2024-03-29 19:03:52 -0300

    atualização final

--- js/sendmail.js
+++ js/sendmail.js
@@ -1,29 +1,5 @@
-var btn = document.getElementById('envarMensagem')
-
-btn.addEventListener('click',()=>{
-    var nome = document.getElementById('nome').value
-    var email = document.getElementById('email').value
-    var telefone = document.getElementById('telefone').value
-    var assunto = document.getElementById('assunto').value
-    var menssagem = document.getElementById('menssagem').value
-
-    if(nome && email && telefone && assunto && menssagem){
-        Email.send({
-            Host : "smtp.elasticemail.com",
-            Username : "me2803390@gmail.com",
-            Password : "60D37F3F9DCFCB84D6988D0BD93D76999C5C",
-            To : 'joaopap1234@gmail.com',
-            From : "me2803390@gmail.com",
-            Subject : `Chegou um email para voce`,
-            Body : `NOME:${nome} <br>\n ASSUNTO:${assunto} <br>\n TELEFONE: ${telefone} <br>\n EMAIL: ${email}<br>\n${menssagem}`
-        }).then(
-            message => alert(message)
-        );
-    }else{
-        alert('Preencha todos os campos')
-    }
-})
-
-
-
-
+var btn=document.getElementById("envarMensagem");btn.addEventListener("click",()=>{var e=document.getElementById("nome").value,a=document.getElementById("email").value,m=document.getElementById("telefone").value,n=document.getElementById("assunto").value,t=document.getElementById("menssagem").value;e&&a&&m&&n&&t?Email.send({Host:"smtp.elasticemail.com",Username:"me2803390@gmail.com",Password:"60D37F3F9DCFCB84D6988D0BD93D76999C5C",To:"joaopap1234@gmail.com",From:"me2803390@gmail.com",Subject:"Chegou um email para voce",Body:`NOME:${e} <br>
+ ASSUNTO:${n} <br>
+ TELEFONE: ${m} <br>
+ EMAIL: ${a}<br>
+${t}`}).then(()=>{alert("sua mensagem foi enviada")}):alert("Preencha todos os campos")});
