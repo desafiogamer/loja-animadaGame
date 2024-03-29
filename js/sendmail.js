b8ef7e217f4e404ce83a53fbb78dd0ce3a238299
desafiogamer <joaopap1234@gmail.com>
2024-03-29 17:01:12 -0300

    atualização final

--- js/sendmail.js
+++ js/sendmail.js
@@ -1,11 +1,12 @@
-var nome = document.getElementById('nome').value
-var email = document.getElementById('email').value
-var telefone = document.getElementById('telefone').value
-var assunto = document.getElementById('assunto').value
-var menssagem = document.getElementById('menssagem').value
 var btn = document.getElementById('envarMensagem')
 
 btn.addEventListener('click',()=>{
+    var nome = document.getElementById('nome').value
+    var email = document.getElementById('email').value
+    var telefone = document.getElementById('telefone').value
+    var assunto = document.getElementById('assunto').value
+    var menssagem = document.getElementById('menssagem').value
+
     if(nome && email && telefone && assunto && menssagem){
         Email.send({
             Host : "smtp.elasticemail.com",
@@ -13,8 +14,10 @@ btn.addEventListener('click',()=>{
             Password : "60D37F3F9DCFCB84D6988D0BD93D76999C5C",
             To : 'joaopap1234@gmail.com',
             From : "me2803390@gmail.com",
-            Subject : `nome:${nome}  assunto:${assunto}`,
-            Body : ` telefone:${telefone} email:${email}
+            Subject : `nome:${nome}`,
+            Body : `assunto:${assunto}
+            telefone:${telefone} 
+            email:${email}
             ${menssagem}`
         }).then(
             message => alert(message)
